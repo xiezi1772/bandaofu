dc717f9384e10806434de7cdb78f2b4c0e61774e
xiezi1772 <1209505591@qq.com>
2017-12-07 21:44:07 +0800

    end

--- js/base.js
+++ js/base.js
@@ -1,3 +1,4 @@
+// 随着浏览器大小改变内容大小
 (function (doc, win) {
 	var docEl = doc.documentElement,
 		resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
@@ -15,3 +16,39 @@
 	win.addEventListener(resizeEvt, recalc, false);
 	doc.addEventListener('DOMContentLoaded', recalc, false);
 })(document, window);
+
+
+$(function(){
+	// 初始化
+	init();
+
+	$(window).resize(function(){
+		cssChange();
+	});
+	
+	
+
+})
+
+function init()
+{
+	// 调整css
+	cssChange();
+
+}
+
+
+
+
+// 初始化css
+function cssChange()
+{
+	// 控制logo区域距离上方的top值
+	var boxHeight = $('header .box').height()+1;
+	$('#logo').css('margin-top', boxHeight+'px');
+	// 控制固定文字
+	$('#text').css('top', boxHeight+'px');
+	// 控制底部距离
+	var bottomH = $('.bottom').height();
+	$('footer .copyright').css('margin-bottom', bottomH + 'px');
+}
